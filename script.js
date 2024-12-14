0e5ee5af240a864956d802c9f37cf77564810343
Георгий Медведский <mathias.frere@yandex.ru>
2024-12-14 03:53:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 const cocktails = [
     {
         name: "Martini Fiero Tonic",
-        image: "./img/martini-fiero.webp",
+        image: "./martini-fiero.webp",
         method: "Build",
         steps: ["Martini Fiero - 50 ml", "Tonic ~ 150 ml", "Долька апельсина", "Лед"]
     },
     {
         name: "Vachego",
-        image: "./img/vachego.jpg",
+        image: "./vachego.jpg",
          method: "Build",
         steps: ["Gin - 50 ml", "Cordial Blueberry - 50 ml", "Сок лимона - 10 ml", "Долька лимона", "Лед"]
     },
     {
         name: "Aperol Spritz",
-        image: "./img/aperol.webp",
+        image: "./aperol.webp",
          method: "Buld",
         steps: ["Aperol - 50 ml", "Sparkling wine - 80 ml", "Soda - 20 ml", "Долька апельсина", "Лед"]
     },
     {
         name: "Porn Star Martini",
-        image: "./img/porn-star.webp",
+        image: "./porn-star.webp",
          method: "Shake",
         steps: ["Водка - 50 ml", "Сок лимона - 20 ml", "Сироп Ваниль - 30 ml", "Пюре маракуйя - 40 ml", "Белок - 10 ml", "Подавать безо льда с шотом игристого"]
     },
     {
         name: "White Pear",
-        image: "./img/white-pear.webp",
+        image: "./white-pear.webp",
          method: "Build",
         steps: ["Ром - 50 ml", "Сок лимона - 20 ml", "Сироп Груша - 30 ml", "Пюре маракуйя - 40 ml", "Белое вино - 50 ml", "Долька апельсина", "Лед"]
     },
     {
         name: "Linchburg Lemonade",
-        image: "./img/linchburg.webp",
+        image: "./linchburg.webp",
          method: "Build",
         steps: ["Бурбон - 50 ml", "Сок лимона - 20 ml", "Сироп Сахар - 20 ml", "Cointreau / Triple Sec - 20 ml", "Soda - 40 ml", "Лед"]
     },
     {
         name: "Basil Smash",
-        image: "./img/basil.webp",
+        image: "./basil.webp",
          method: "Build",
          steps: ["Gin - 50 ml", "Cordial Basil - 50 ml", "Сок лимона - 10 ml", "Долька лимона", "Лед"]
     },
     {
         name: "Passion Sour",
-        image: "./img/pasion.webp",
+        image: "./pasion.webp",
          method: "Shake",
          steps: ["Бурбон - 50 ml", "Сок лимона - 25 ml", "Сироп Сахар - 25 ml", "Пюре маракуйя - 40 ml", "Белок - 10 ml", "Долька лимона", "Подавать со льдом в роксе"]
     },
     {
         name: "Negroni",
-        image: "./img/negroni.webp",
+        image: "./negroni.webp",
          method: "Build",
         steps: ["Campari - 30 ml", "Вермут Rosso - 30 ml", "Gin - 30 ml", "Долька апельсина", "Лед"]
     },
     {
         name: "Bramble Tonic",
-        image: "./img/gin-tonic.webp",
+        image: "./gin-tonic.webp",
          method: "Build",
         steps: ["Gin - 50 ml", "Tonic - 150 ml", "Долька лимона", "Лед"]
     },
     {
         name: "Spritz Strawberry & Hibiscus",
-        image: "./img/spritz-strawberry.webp",
+        image: "./spritz-strawberry.webp",
          method: "Build",
          steps: ["Gin / Rum / Vodka - 50 ml", "Rocket tonic - 50 ml", "Сок лимона - 10 ml", "Сироп Клубника - 20 ml", "Tonic - 100 ml", "Долька лимона", "Лед"]
     }
